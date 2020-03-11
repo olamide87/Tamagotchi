e6cef98ace7d1a093e4250e29b75a71c845b8954
Olamide Oladinni <olajide1987@gmail.com>
2020-03-10 20:28:26 -0500

    eat finished

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,29 @@
-import eat from "./Component/eat/eat.js";
+import eat from './Component/eat/eat';
+import eatData from './helpers/data/eatData';
 
+const addToFullScore = () => {
+  eatData.setFullScore(10);
+  eat.eatBuilder();
+};
+
+const subFromFullScore = () => {
+  eatData.setFullScore(-3);
+  eat.eatBuilder();
+};
+
+
+const buttonEvents = () => {
+  $('#eat').on('click', '#healthy-food-btn', addToFullScore);
+  $('#eat').on('click', '#unhealthy-food-btn', subFromFullScore);
+};
+
+const quad = () => {
+  eat.eatBuilder();
+};
 
 const init = () => {
-  eat.printProgress();
+  buttonEvents();
+  quad();
 };
 
 init();
